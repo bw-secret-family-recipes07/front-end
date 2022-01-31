b992b99f30bdaecaed416a51c97bf881e83f98e0
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-01-31 18:09:38 -0500

    created Logout component and pseudocoded the useEffect

---
+++ src/components/Logout.js
@@ -0,0 +1,23 @@
+import React from 'react'; //need to include useEffect
+// import { useHistory } from 'react-router-dom';
+//import axiosWithAuth
+
+const Logout = () => {
+//const { push } = useHistory();
+
+//useEffect(() => {
+//     axiosWithAuth()
+//     .post('/logout')
+//     .then(res => {
+//         localStorage.removeItem('token');
+//         push('/login');
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+// }, [])
+
+    return(<div></div>);
+}
+
+export default Logout;
