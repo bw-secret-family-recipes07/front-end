5c4400a42745176b8f1290e61a6a96ab1f89ea45
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-01-31 19:08:54 -0500

    DOM complete for create account form

--- src/components/CreateAccountForm.js
+++ src/components/CreateAccountForm.js
@@ -1,5 +1,45 @@
 import React from 'react';
 
 export default function CreateAccountForm() {
-  return <div></div>;
+  return (
+    <div>
+        <h1>Secret Family Recipe Cookbook</h1>
+        <div className='form'>
+            <h2>Enter your Information below to Create your Account</h2>
+            <form>
+                <label>First Name:</label>
+                <input 
+                type='text'
+                name='first-name'
+                value='first-name'
+                />
+                 <label>Last Name:</label>
+                <input 
+                type='text'
+                name='last-name'
+                value='last-name'
+                />
+                 <label>Email:</label>
+                <input 
+                type='email'
+                name='email'
+                value='email'
+                />
+                 <label>Username:</label>
+                <input 
+                type='text'
+                name='username'
+                value='username'
+                />
+                 <label>Password:</label>
+                <input 
+                type='password'
+                name='password'
+                value='password'
+                />
+                <button>Create Account</button>
+            </form>
+            <button>Login</button>
+        </div>
+    </div>);
 }
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import Recipe from './Recipe';
+// import Recipe from './Recipe';
 
 export default function RecipeList() {
   return (
@@ -15,7 +15,17 @@ export default function RecipeList() {
             <button>Add New Recipe +</button>
         </div>
         <div className='recipes'>
-          <Recipe />
+            {/* {
+                recipes.map(recipe =>{
+                    return (<Link to='/recipe/{id}'><img src={recipe.img} /></Link>);
+                })
+            } */}
+        <img src='https://media.istockphoto.com/photos/grilled-chicken-meat-and-fresh-vegetable-salad-of-tomato-avocado-and-picture-id1295633127?b=1&k=20&m=1295633127&s=170667a&w=0&h=VDkBqjm0RShberDPMJ_L-LHX1rZ5v8yNvq0I0UxXquM=' alt='chicken salad' />
+        <img src='https://media.istockphoto.com/photos/grilled-chicken-meat-and-fresh-vegetable-salad-of-tomato-avocado-and-picture-id1295633127?b=1&k=20&m=1295633127&s=170667a&w=0&h=VDkBqjm0RShberDPMJ_L-LHX1rZ5v8yNvq0I0UxXquM=' alt='chicken salad' />
+        <img src='https://media.istockphoto.com/photos/grilled-chicken-meat-and-fresh-vegetable-salad-of-tomato-avocado-and-picture-id1295633127?b=1&k=20&m=1295633127&s=170667a&w=0&h=VDkBqjm0RShberDPMJ_L-LHX1rZ5v8yNvq0I0UxXquM=' alt='chicken salad' />
+        <img src='https://media.istockphoto.com/photos/grilled-chicken-meat-and-fresh-vegetable-salad-of-tomato-avocado-and-picture-id1295633127?b=1&k=20&m=1295633127&s=170667a&w=0&h=VDkBqjm0RShberDPMJ_L-LHX1rZ5v8yNvq0I0UxXquM=' alt='chicken salad' />
+        <img src='https://media.istockphoto.com/photos/grilled-chicken-meat-and-fresh-vegetable-salad-of-tomato-avocado-and-picture-id1295633127?b=1&k=20&m=1295633127&s=170667a&w=0&h=VDkBqjm0RShberDPMJ_L-LHX1rZ5v8yNvq0I0UxXquM=' alt='chicken salad' />
+        <img src='https://media.istockphoto.com/photos/grilled-chicken-meat-and-fresh-vegetable-salad-of-tomato-avocado-and-picture-id1295633127?b=1&k=20&m=1295633127&s=170667a&w=0&h=VDkBqjm0RShberDPMJ_L-LHX1rZ5v8yNvq0I0UxXquM=' alt='chicken salad' />
         </div>
     </div>);
 }
